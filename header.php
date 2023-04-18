763377ae1051b14629d28b0b1264a6efdce4042d
Leo QUILLOT <quillot.leo@orange.fr>
2023-04-18 15:07:44 +0200

    Comment

--- header.php
+++ header.php
@@ -1,73 +1,112 @@
-
-
 <?php
- //echo " <div style=\"color:#ff00ff;\"> GET<pre>", print_r ( $_GET ), "</pre></div>";
-$liste = ! empty($liste) ? $liste : null;
+// Do something taht I don't understand
 require_once ('./mysql/ControleurConnexion.php');
-$connexion = new ControleurConnexion();
 
+// Variables declaration
+$Nom = isset($_GET['Nom']) ? $_GET['Nom'] : NULL;
+$contenu_page = null;
+$liste = ! empty($liste) ? $liste : null;
+$connexion = new ControleurConnexion();
 $str = file_get_contents('data/laposte_hexasmal.json');
 $json = json_decode($str, true); // decode the JSON into an associative array
 $code_postal_array = array();
 
+// Fill the dictionary that associate 'code_postal' and 'town' and sort it by 'code_postal'
 for($i = 0; $i<sizeof($json); $i++)
 {	
-	//$code = substr($json[$i]['fields']['code_postal'], 0, -1);
 	$code = $json[$i]['fields']['code_postal'];
 	$town = $json[$i]['fields']['nom_de_la_commune'];
-	//echo " <div style=\"color:#ff00ff;\"> GET<pre>", print_r ( $json[$i]['fields'] ), "</pre></div>";
 	if(!in_array($code, $code_postal_array))
 	{
 		$code_postal_array[$code] = array();
 	}
 	array_push($code_postal_array[$code], $town);
-
 }
-
 ksort($code_postal_array);
 
-foreach (array_keys($code_postal_array) as $option) {
-	echo " <div style=\"color:#ff00ff;\"> GET<pre>", print_r ( $option ), print_r($code_postal_array[$option]), "</pre></div>";
-}
-
-$Nom = isset($_GET['Nom']) ? $_GET['Nom'] : NULL;
+// Do something taht I don't understand
 if (! empty($Nom)) {
       $connexion->inserer("test", "Id,Nom", "NULL,'$Nom'");
 }
-//$liste_personnels = $connexion->consulter("Nom,Prenom,Naiss,Adresse,CodePostal,Ville,Telephone,Mail,Secu", "test", "", "", "", "", "", "");
-//$i = 0;
-//foreach ($liste_personnels as $data) {
-	//$i ++;
-	/*
-	$Nom = ! empty($data['Nom']) ? utf8_encode($data['Nom']) : "";
-	$Prenom = ! empty($data['Prenom']) ? utf8_encode($data['Prenom']) : "";
-	$Date = ! empty($data['Naiss']) ? utf8_encode($data['Naiss']) : "";
-	$Adresse = ! empty($data['Adresse']) ? utf8_encode($data['Adresse']) : "";
-	$CodePostal = ! empty($data['CodePostal']) ? utf8_encode($data['CodePostal']) : "";
-	$Ville = ! empty($data['Ville']) ? utf8_encode($data['Ville']) : "";
-	$Telephone = ! empty($data['Telephone']) ? utf8_encode($data['Telephone']) : "";
-	$Mail = ! empty($data['Mail']) ? utf8_encode($data['Mail']) : "";
-	$Secu = ! empty($data['Secu']) ? utf8_encode($data['Secu']) : "";
-    $liste .= <<<EOF
-    <tr>
-    	<td>$Nom $Prenom</td>
-		<td>$Date</td>
-		<td>$Adresse, $CodePostal, $Ville</td>
-		<td>$Telephone</td>
-		<td>$Mail</td>
-		<td>$Secu</td>
-		<td>
-			<div class="btn-group">
-				<input type="button" onclick="edit()" value="Modif"/>
-				<input type="button" onclick="suppr()" value="Suppr"/>
-			</div>
-		</td>
-    </tr>
-    EOF;
-}*/
-
 ?>
-//Application de test SI - Leo QUILLOT & Felix DELESALLE
+
+<script>
+	// Variables declaration
+	const js_code_postal_array = [<?php echo '"'.implode('","',  array_keys($code_postal_array) ).'"' ?>]
+	const dlOptions = js_code_postal_array.map(o => {return [`<option value="${o}"></option>`, o.toLowerCase()];});
+
+	function completeDataList(e, tag, list_tag, max_ch=99999)
+	// tag : str, the html tag of the input
+	// list_tag : str, the html tag of the completion list
+	// max_ch : int, the maximum number of character in the text input
+	{
+		var element =document.getElementById(tag)
+		if(element)
+		{
+			var text = document.getElementById(tag).value;
+    		const fill = val => document.getElementById(list_tag).innerHTML = val;
+    		if(!e.target.value) 
+			{
+        		fill(dlOptions.reduce((sum, [html]) => sum + html, ''));
+    		} 
+			else if(!(e instanceof InputEvent)) { // OR: else if(!e.inputType)
+        		e.target.blur();
+    		} 
+			else 
+			{
+        		const inputValue = e.target.value.toLowerCase();
+        		let result = '';
+				if(text.length < max_ch)
+				{
+        			for (const [html, valuePattern] of dlOptions) 
+					{
+            			if (!valuePattern.indexOf(inputValue)) 
+						{
+            	    		result += html;
+            			} 
+						else if (result) 
+						{
+            	    		break;
+            			}
+        			}
+				}
+        		fill(result);
+    		}
+		}
+
+	}
+
+	// Function called by text input when focus in, fill the list of possible completion
+	function fillDataListIfEmpty(tag, list_tag, max_ch=99999) 
+	// tag : str, the html tag of the input
+	// list_tag : str, the html tag of the completion list
+	// max_ch : int, the maximum number of character in the text input
+	{
+		var text = document.getElementById(tag).value;
+    	if(!document.getElementById(list_tag).innverHTML && text.length < max_ch) 
+		{
+        	completeDataList({ target: {} });
+    	}
+	}
+
+	// Function called by text input when focus out, changes the style depending of the validity of the content
+	function checkValidState(tag, max_ch=99999)
+	// tag : str, the html tag of the input
+	// max_ch : int, the maximum number of character in the text input
+	{
+		var text = document.getElementById(tag).value;
+		if(text.length < max_ch || !js_code_postal_array.includes(text))
+		{
+			document.getElementById(tag).style.backgroundColor = "red";
+		}
+		else
+		{
+			document.getElementById(tag).style.backgroundColor = null;
+		}
+	}
+	</script>
+
+Application de test SI - Leo QUILLOT & Felix DELESALLE
 <form action="index.php" method="GET" autocomplete="off" enctype="application/x-www-form-urlencoded">
 <div>
 	<p>Saisir le nom</p>
@@ -84,67 +123,8 @@ if (! empty($Nom)) {
 <div>
 	<p>Saisir adresse</p>
 	<p><input type="text" name="Adresse"  /></p>
-	<script>
-		const js_code_postal_array = [<?php echo '"'.implode('","',  array_keys($code_postal_array) ).'"' ?>]
-	const dlOptions = js_code_postal_array.map(o => {
-    return [`<option value="${o}"></option>`, o.toLowerCase()];
-	});
-
-	function completeDataList(e, tag, list_tag, max_ch=99999) {
-		var text = document.getElementById(tag).value;
-    	const fill = val => document.getElementById(list_tag).innerHTML = val;
-    	if(!e.target.value) 
-		{
-        	fill(dlOptions.reduce((sum, [html]) => sum + html, ''));
-    	} 
-		else if(!(e instanceof InputEvent)) { // OR: else if(!e.inputType)
-        	e.target.blur();
-    	} 
-		else 
-		{
-        	const inputValue = e.target.value.toLowerCase();
-        	let result = '';
-			if(text.length < max_ch)
-			{
-        		for (const [html, valuePattern] of dlOptions) 
-				{
-            		if (!valuePattern.indexOf(inputValue)) 
-					{
-                		result += html;
-            		} 
-					else if (result) 
-					{
-                		break;
-            		}
-        		}
-			}
-        	fill(result);
-    	}
-	}
-
-	function fillDataListIfEmpty(tag, list_tag, max_ch=99999) {
-		var text = document.getElementById(tag).value;
-    	if(!document.getElementById(list_tag).innverHTML && text.length < max_ch) 
-		{
-        	completeDataList({ target: {} });
-    	}
-	}
-
-	function checkValidState(tag, max_ch=99999) {
-		var text = document.getElementById(tag).value;
-		if(text.length < max_ch || !js_code_postal_array.includes(text))
-		{
-			document.getElementById(tag).style.backgroundColor = "red";
-		}
-		else
-		{
-			document.getElementById(tag).style.backgroundColor = null;
-		}
-	}
-	</script>
 	<input type="text" name="CodePostal" minlength="5" maxlength="5" required id="code_postal_tag" list="code_postal_list" oninput="completeDataList(event, 'code_postal_tag', 'code_postal_list', 5)" onfocus="fillDataListIfEmpty('code_postal_tag', 'code_postal_list', 5)" onfocusout="checkValidState('code_postal_tag', 5)">
 	<datalist id="code_postal_list"></datalist>
-
     <p><input type="text" name="Ville"  /></p>
 </div>
 <div>
@@ -164,6 +144,8 @@ if (! empty($Nom)) {
 </div>
 </form>
 <div>
+
+
 <?php
 echo $contenu_page;
 ?>
